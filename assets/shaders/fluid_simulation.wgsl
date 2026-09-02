4ef4ede48ca0ca921314bd43b157584604f6606e
Jeff Barfield <jeffreybarfield@protonmail.com>
2026-09-01 21:28:21 -0400

    Add a diffuse compute group; clean up some cod

--- assets/shaders/fluid_simulation.wgsl
+++ assets/shaders/fluid_simulation.wgsl
@@ -4,16 +4,21 @@
 // Two textures are needed for the game of life as each pixel of step N depends on the state of its
 // neighbors at step N-1.
 
-@group(0) @binding(0) var input: texture_storage_2d<rgba32float, read>;
+@group(0) @binding(0) var original: texture_storage_2d<rgba32float, read>;
 
-@group(0) @binding(1) var output: texture_storage_2d<rgba32float, write>;
+@group(0) @binding(1) var current: texture_storage_2d<rgba32float, read>;
 
-@group(0) @binding(2) var<uniform> config: GameOfLifeUniforms;
+@group(0) @binding(2) var next: texture_storage_2d<rgba32float, write>;
 
-struct GameOfLifeUniforms {
+
+@group(0) @binding(3) var<uniform> config: FluidSimulationUniforms;
+
+struct FluidSimulationUniforms {
     alive_color: vec4<f32>,
     cursor_position: vec2<f32>,
+    cursor_density: f32,
     time: f32,
+    diff: f32,
     dimensions: vec2<f32>,
 }
 
@@ -34,18 +39,23 @@ fn randomFloat(value: u32) -> f32 {
 
 @compute @workgroup_size(8, 8, 1)
 fn init(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
+    if (invocation_id.x >= u32(config.dimensions.x) ||
+        invocation_id.y >= u32(config.dimensions.y)) {
+        return;
+    }
+
     let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
 
     let randomNumber = randomFloat((invocation_id.y << 16u) | invocation_id.x);
     let alive = randomNumber > 0.9;
     // Use alpha channel to keep track of cell's state
-    let color = vec4(config.alive_color.rgb, f32(alive));
+    let color = vec4(0., 0., 0., 1);
 
-    textureStore(output, location, color);
+    textureStore(next, location, color);
 }
 
 fn is_alive(location: vec2<i32>, offset_x: i32, offset_y: i32) -> i32 {
-    let value: vec4<f32> = textureLoad(input, location + vec2<i32>(offset_x, offset_y));
+    let value: vec4<f32> = textureLoad(current, location + vec2<i32>(offset_x, offset_y));
     return i32(value.a);
 }
 
@@ -60,28 +70,137 @@ fn count_alive(location: vec2<i32>) -> i32 {
            is_alive(location,  1,  1);
 }
 
-fn add_source(n: i32, x: vec2<f32>, s: vec2<f32>, dt: f32) {
-    let value: vec4<f32> = textureLoad(input, location + vec2<i32>(offset_x, offset_y));
+fn density_source_at(location: vec2<i32>) -> f32 {
+    let cursor = config.cursor_position * config.dimensions;
+    let p = vec2<f32>(location);
+
+    let distance = length(p - cursor);
+
+    let radius = 5.0;
+
+    let strength = 1.0 - clamp(
+        distance / radius,
+        0.0,
+        1.0
+    );
+
+    return config.cursor_density * strength;
+}
+
+fn set_bnd(location: vec2<i32>, value: vec4<f32>, b: i32) -> vec4<f32> {
+    let width = i32(config.dimensions.x);
+    let height = i32(config.dimensions.y);
+
+    var result = value;
+
+    // Left boundary
+    if (location.x == 0) {
+        let neighbor = textureLoad(current, vec2<i32>(1, location.y));
+
+        result.r = select(
+            neighbor.r,
+            -neighbor.r,
+            b == 1
+        );
+    }
+
+    // Right boundary
+    else if (location.x == width - 1) {
+        let neighbor = textureLoad(
+            current,
+            vec2<i32>(width - 2, location.y)
+        );
+
+        result.r = select(
+            neighbor.r,
+            -neighbor.r,
+            b == 1
+        );
+    }
+
+    // Bottom boundary
+    else if (location.y == 0) {
+        let neighbor = textureLoad(current, vec2<i32>(location.x, 1));
+
+        result.r = select(
+            neighbor.r,
+            -neighbor.r,
+            b == 2
+        );
+    }
+
+    // Top boundary
+    else if (location.y == height - 1) {
+        let neighbor = textureLoad(
+            current,
+            vec2<i32>(location.x, height - 2)
+        );
+
+        result.r = select(
+            neighbor.r,
+            -neighbor.r,
+            b == 2
+        );
+    }
+
+    return result;
+}
+
+@compute @workgroup_size(8, 8, 1)
+fn diffuse(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
+  // JAB TODO: Why is this check necessary
+  if (invocation_id.x >= u32(config.dimensions.x) ||
+      invocation_id.y >= u32(config.dimensions.y)) {
+      return;
+  }
+
+  let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
+
+  let width = i32(config.dimensions.x);
+  let height = i32(config.dimensions.y);
+
+  if location.x == 0 ||
+     location.y == 0 ||
+     location.x == width - 1 ||
+     location.y == height - 1 {
+     return;
+  }
+
+  let n = config.dimensions.x - 2.0;
+  let a = config.time * config.diff * n * n;
+
+  var x0 = textureLoad(original, location);
+
+  let left = textureLoad(current, location + vec2<i32>(-1, 0));
+  let right = textureLoad(current, location + vec2<i32>(1, 0));
+  let below = textureLoad(current, location + vec2<i32>(0, -1));
+  let above = textureLoad(current, location + vec2<i32>(0, 1));
+
+  var result = textureLoad(current, location);
+
+  result.r = (
+      x0.r +
+      a * (
+          left.r +
+          right.r +
+          below.r +
+          above.r
+      )
+  ) / (1.0 + 4.0 * a);
+
+  textureStore(next, location, result);
 }
 
 @compute @workgroup_size(8, 8, 1)
 fn update(@builtin(global_invocation_id) invocation_id: vec3<u32>) {
-    //  let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
-    //
-    //  let n_alive = count_alive(location);
-    //
-    //  var alive: bool;
-    //  if (n_alive == 3) {
-    //      alive = true;
-    //  } else if (n_alive == 2) {
-    //      let currently_alive = is_alive(location, 0, 0);
-    //      alive = bool(currently_alive);
-    //  } else {
-    //      alive = false;
-    //  }
-    // let color = vec4(config.alive_color.rgb, f32(alive));
+    if (invocation_id.x >= u32(config.dimensions.x) ||
+        invocation_id.y >= u32(config.dimensions.y)) {
+        return;
+    }
 
     let location = vec2<i32>(i32(invocation_id.x), i32(invocation_id.y));
-    let color = vec4(config.alive_color.rgb, f32(is_alive(location, 0, 0)));
-    textureStore(output, location, color);
+    var color = textureLoad(current, location);
+    color.r += config.time * density_source_at(location);
+    color = set_bnd(location, color, 0);
+    textureStore(next, location, color);
 }
--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ use std::borrow::Cow;
 const SHADER_ASSET_PATH: &str = "shaders/fluid_simulation.wgsl";
 
 const DISPLAY_FACTOR: u32 = 4;
-const SIZE: UVec2 = UVec2::new(1280 / DISPLAY_FACTOR, 720 / DISPLAY_FACTOR);
+const SIZE: UVec2 = UVec2::new((1280 / DISPLAY_FACTOR) + 2, (1280 / DISPLAY_FACTOR) + 2);
 const WORKGROUP_SIZE: u32 = 8;
 
 fn main() {
@@ -44,7 +44,7 @@ fn main() {
                     ..default()
                 })
                 .set(ImagePlugin::default_nearest()),
-            GameOfLifeComputePlugin,
+            FluidSimComputePlugin,
         ))
         .add_systems(Startup, setup)
         .add_systems(Update, switch_textures)
@@ -57,12 +57,13 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
     image.asset_usage = RenderAssetUsages::RENDER_WORLD;
     image.texture_descriptor.usage =
         TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
-    let image0 = images.add(image.clone());
-    let image1 = images.add(image);
+    let original = images.add(image.clone());
+    let current = images.add(image.clone());
+    let next = images.add(image);
 
     commands.spawn((
         Sprite {
-            image: image0.clone(),
+            image: next.clone(),
             custom_size: Some(SIZE.as_vec2()),
             ..default()
         },
@@ -70,84 +71,89 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
     ));
     commands.spawn(Camera2d);
 
-    commands.insert_resource(GameOfLifeImages {
-        texture_a: image0,
-        texture_b: image1,
+    commands.insert_resource(FluidSimImages {
+        original,
+        current,
+        next,
     });
 
-    commands.insert_resource(GameOfLifeUniforms {
+    commands.insert_resource(FluidSimUniforms {
         alive_color: LinearRgba::RED,
         cursor_position: Vec2::ZERO,
+        cursor_density: 10.,
         time: 0.0,
+        diff: 0.0001,
         dimensions: SIZE.as_vec2(),
     });
 }
 
 // Switch texture to display every frame to show the one that was written to most recently.
-fn switch_textures(images: Res<GameOfLifeImages>, mut sprite: Single<&mut Sprite>) {
-    if sprite.image == images.texture_a {
-        sprite.image = images.texture_b.clone();
+fn switch_textures(images: Res<FluidSimImages>, mut sprite: Single<&mut Sprite>) {
+    if sprite.image == images.current {
+        sprite.image = images.next.clone();
     } else {
-        sprite.image = images.texture_a.clone();
+        sprite.image = images.current.clone();
     }
 }
 
-struct GameOfLifeComputePlugin;
+struct FluidSimComputePlugin;
 
-impl Plugin for GameOfLifeComputePlugin {
+impl Plugin for FluidSimComputePlugin {
     fn build(&self, app: &mut App) {
         // Extract the game of life image resource from the main world into the render world
         // for operation on by the compute shader and display on the sprite.
         app.add_plugins((
-            ExtractResourcePlugin::<GameOfLifeImages>::default(),
-            ExtractResourcePlugin::<GameOfLifeUniforms>::default(),
+            ExtractResourcePlugin::<FluidSimImages>::default(),
+            ExtractResourcePlugin::<FluidSimUniforms>::default(),
         ));
         let render_app = app.sub_app_mut(RenderApp);
         render_app
-            .init_resource::<GameOfLifeState>()
-            .add_systems(RenderStartup, init_game_of_life_pipeline)
+            .init_resource::<FluidSimState>()
+            .add_systems(RenderStartup, init_fluid_sim_pipeline)
             .add_systems(
                 Render,
                 prepare_bind_group.in_set(RenderSystems::PrepareBindGroups),
             )
             .add_systems(Render, update.in_set(RenderSystems::Prepare))
-            .add_systems(RenderGraph, game_of_life.before(camera_driver));
+            .add_systems(RenderGraph, fluid_simulation.before(camera_driver));
     }
 }
 
 #[derive(Resource, Clone, ExtractResource)]
-struct GameOfLifeImages {
-    texture_a: Handle<Image>,
-    texture_b: Handle<Image>,
+struct FluidSimImages {
+    original: Handle<Image>,
+    current: Handle<Image>,
+    next: Handle<Image>,
 }
 
 #[derive(Resource, Clone, ExtractResource, ShaderType)]
-struct GameOfLifeUniforms {
+struct FluidSimUniforms {
     alive_color: LinearRgba,
     cursor_position: Vec2,
+    cursor_density: f32,
     time: f32,
+    diff: f32,
     dimensions: Vec2,
 }
 
 #[derive(Resource)]
-struct GameOfLifeImageBindGroups([BindGroup; 2]);
+struct FluidSimBindGroups([BindGroup; 2]);
 
 fn prepare_bind_group(
     mut commands: Commands,
-    pipeline: Res<GameOfLifePipeline>,
+    pipeline: Res<FluidSimPipeline>,
     gpu_images: Res<RenderAssets<GpuImage>>,
-    game_of_life_images: Res<GameOfLifeImages>,
-    game_of_life_uniforms: Res<GameOfLifeUniforms>,
+    fluid_sim_images: Res<FluidSimImages>,
+    fluid_sim_uniforms: Res<FluidSimUniforms>,
     render_device: Res<RenderDevice>,
     pipeline_cache: Res<PipelineCache>,
     queue: Res<RenderQueue>,
 ) {
-    let view_a = gpu_images.get(&game_of_life_images.texture_a).unwrap();
-    let view_b = gpu_images.get(&game_of_life_images.texture_b).unwrap();
+    let view_a = gpu_images.get(&fluid_sim_images.original).unwrap();
+    let view_b = gpu_images.get(&fluid_sim_images.current).unwrap();
+    let view_c = gpu_images.get(&fluid_sim_images.next).unwrap();
 
-    // Uniform buffer is used here to demonstrate how to set up a uniform in a compute shader
-    // Alternatives such as storage buffers or push constants may be more suitable for your use case
-    let mut uniform_buffer = UniformBuffer::from(game_of_life_uniforms.into_inner());
+    let mut uniform_buffer = UniformBuffer::from(fluid_sim_uniforms.into_inner());
     uniform_buffer.write_buffer(&render_device, &queue);
 
     let bind_group_0 = render_device.create_bind_group(
@@ -156,6 +162,7 @@ fn prepare_bind_group(
         &BindGroupEntries::sequential((
             &view_a.texture_view,
             &view_b.texture_view,
+            &view_c.texture_view,
             &uniform_buffer,
         )),
     );
@@ -163,34 +170,37 @@ fn prepare_bind_group(
         None,
         &pipeline_cache.get_bind_group_layout(&pipeline.texture_bind_group_layout),
         &BindGroupEntries::sequential((
-            &view_b.texture_view,
             &view_a.texture_view,
+            &view_c.texture_view,
+            &view_b.texture_view,
             &uniform_buffer,
         )),
     );
-    commands.insert_resource(GameOfLifeImageBindGroups([bind_group_0, bind_group_1]));
+    commands.insert_resource(FluidSimBindGroups([bind_group_0, bind_group_1]));
 }
 
 #[derive(Resource)]
-struct GameOfLifePipeline {
+struct FluidSimPipeline {
     texture_bind_group_layout: BindGroupLayoutDescriptor,
     init_pipeline: CachedComputePipelineId,
     update_pipeline: CachedComputePipelineId,
+    diffuse_pipeline: CachedComputePipelineId,
 }
 
-fn init_game_of_life_pipeline(
+fn init_fluid_sim_pipeline(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     pipeline_cache: Res<PipelineCache>,
 ) {
     let texture_bind_group_layout = BindGroupLayoutDescriptor::new(
-        "GameOfLifeImages",
+        "FluidSimImages",
         &BindGroupLayoutEntries::sequential(
             ShaderStages::COMPUTE,
             (
                 texture_storage_2d(TextureFormat::Rgba32Float, StorageTextureAccess::ReadOnly),
+                texture_storage_2d(TextureFormat::Rgba32Float, StorageTextureAccess::ReadOnly),
                 texture_storage_2d(TextureFormat::Rgba32Float, StorageTextureAccess::WriteOnly),
-                uniform_buffer::<GameOfLifeUniforms>(false),
+                uniform_buffer::<FluidSimUniforms>(false),
             ),
         ),
     );
@@ -203,20 +213,27 @@ fn init_game_of_life_pipeline(
     });
     let update_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
         layout: vec![texture_bind_group_layout.clone()],
-        shader,
+        shader: shader.clone(),
         entry_point: Some(Cow::from("update")),
         ..default()
     });
+    let diffuse_pipeline = pipeline_cache.queue_compute_pipeline(ComputePipelineDescriptor {
+        layout: vec![texture_bind_group_layout.clone()],
+        shader,
+        entry_point: Some(Cow::from("diffuse")),
+        ..default()
+    });
 
-    commands.insert_resource(GameOfLifePipeline {
+    commands.insert_resource(FluidSimPipeline {
         texture_bind_group_layout,
         init_pipeline,
         update_pipeline,
+        diffuse_pipeline,
     });
 }
 
 #[derive(Resource, Default)]
-enum GameOfLifeState {
+enum FluidSimState {
     #[default]
     Loading,
     Init,
@@ -224,16 +241,16 @@ enum GameOfLifeState {
 }
 
 fn update(
-    pipeline: Res<GameOfLifePipeline>,
+    pipeline: Res<FluidSimPipeline>,
     pipeline_cache: Res<PipelineCache>,
-    mut state: ResMut<GameOfLifeState>,
+    mut state: ResMut<FluidSimState>,
 ) {
     // if the corresponding pipeline has loaded, transition to the next stage
     match *state {
-        GameOfLifeState::Loading => {
+        FluidSimState::Loading => {
             match pipeline_cache.get_compute_pipeline_state(pipeline.init_pipeline) {
                 CachedPipelineState::Ok(_) => {
-                    *state = GameOfLifeState::Init;
+                    *state = FluidSimState::Init;
                 }
                 // If the shader hasn't loaded yet, just wait.
                 CachedPipelineState::Err(ShaderCacheError::ShaderNotLoaded(_)) => {}
@@ -243,73 +260,104 @@ fn update(
                 _ => {}
             }
         }
-        GameOfLifeState::Init => {
+        FluidSimState::Init => {
             if let CachedPipelineState::Ok(_) =
                 pipeline_cache.get_compute_pipeline_state(pipeline.update_pipeline)
             {
-                *state = GameOfLifeState::Update(1);
+                *state = FluidSimState::Update(1);
             }
         }
-        GameOfLifeState::Update(0) => {
-            *state = GameOfLifeState::Update(1);
+        FluidSimState::Update(0) => {
+            *state = FluidSimState::Update(1);
         }
-        GameOfLifeState::Update(1) => {
-            *state = GameOfLifeState::Update(0);
+        FluidSimState::Update(1) => {
+            *state = FluidSimState::Update(0);
         }
-        GameOfLifeState::Update(_) => unreachable!(),
+        FluidSimState::Update(_) => unreachable!(),
     }
 }
 
-fn game_of_life(
+fn fluid_simulation(
     mut render_context: RenderContext,
-    bind_groups: Res<GameOfLifeImageBindGroups>,
+    bind_groups: Res<FluidSimBindGroups>,
     pipeline_cache: Res<PipelineCache>,
-    pipeline: Res<GameOfLifePipeline>,
-    state: Res<GameOfLifeState>,
+    pipeline: Res<FluidSimPipeline>,
+    state: Res<FluidSimState>,
 ) {
     let mut pass = render_context
         .command_encoder()
         .begin_compute_pass(&ComputePassDescriptor::default());
 
-    // select the pipeline based on the current state
     match *state {
-        GameOfLifeState::Loading => {}
-        GameOfLifeState::Init => {
+        FluidSimState::Loading => {}
+
+        FluidSimState::Init => {
             let init_pipeline = pipeline_cache
                 .get_compute_pipeline(pipeline.init_pipeline)
                 .unwrap();
-            pass.set_bind_group(0, &bind_groups.0[0], &[]);
+
             pass.set_pipeline(init_pipeline);
-            pass.dispatch_workgroups(SIZE.x / WORKGROUP_SIZE, SIZE.y / WORKGROUP_SIZE, 1);
+            pass.set_bind_group(0, &bind_groups.0[0], &[]);
+
+            pass.dispatch_workgroups(
+                SIZE.x.div_ceil(WORKGROUP_SIZE),
+                SIZE.y.div_ceil(WORKGROUP_SIZE),
+                1,
+            );
         }
-        GameOfLifeState::Update(index) => {
+
+        FluidSimState::Update(index) => {
             let update_pipeline = pipeline_cache
                 .get_compute_pipeline(pipeline.update_pipeline)
                 .unwrap();
-            pass.set_bind_group(0, &bind_groups.0[index], &[]);
+
             pass.set_pipeline(update_pipeline);
-            pass.dispatch_workgroups(SIZE.x / WORKGROUP_SIZE, SIZE.y / WORKGROUP_SIZE, 1);
+            pass.set_bind_group(0, &bind_groups.0[index], &[]);
+
+            pass.dispatch_workgroups(
+                SIZE.x.div_ceil(WORKGROUP_SIZE),
+                SIZE.y.div_ceil(WORKGROUP_SIZE),
+                1,
+            );
+
+            let diffuse_pipeline = pipeline_cache
+                .get_compute_pipeline(pipeline.diffuse_pipeline)
+                .unwrap();
+
+            pass.set_pipeline(diffuse_pipeline);
+
+            for i in 0..20 {
+                let diffuse_index = (index + 1 + i) % 2;
+
+                pass.set_bind_group(0, &bind_groups.0[diffuse_index], &[]);
+
+                pass.dispatch_workgroups(
+                    SIZE.x.div_ceil(WORKGROUP_SIZE),
+                    SIZE.y.div_ceil(WORKGROUP_SIZE),
+                    1,
+                );
+            }
         }
     }
 }
 
 fn update_cursor_position(
     windows: Query<&Window>,
-    mut uniforms: ResMut<GameOfLifeUniforms>,
+    mut uniforms: ResMut<FluidSimUniforms>,
 ) -> Result {
     let window = windows.single()?;
 
     if let Some(cursor_position) = window.cursor_position() {
-        uniforms.cursor_position = Vec2::new(
-            (cursor_position.x / window.width()).clamp(0.0, 1.0),
-            1.0 - (cursor_position.y / window.height()).clamp(0.0, 1.0),
-        );
+        let texture_size = SIZE.as_vec2() * DISPLAY_FACTOR as f32;
+        let texture_origin = (window.size() - texture_size) * 0.5;
+        let relative = (cursor_position - texture_origin) / texture_size;
+
+        uniforms.cursor_position = relative.clamp(Vec2::ZERO, Vec2::ONE);
     }
     Ok(())
 }
 
-fn update_shader_time(time: Res<Time>, mut uniforms: ResMut<GameOfLifeUniforms>) {
-    println!("{:?}", time.delta().as_secs_f32());
+fn update_shader_time(time: Res<Time>, mut uniforms: ResMut<FluidSimUniforms>) {
     uniforms.time = time.delta().as_secs_f32();
     uniforms.dimensions = SIZE.as_vec2();
 }
